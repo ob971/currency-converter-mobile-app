5f0575c26f5e00d28fa23b42c8e6356260013fb6
obsa <obsukebede2022@gmail.com>
2023-10-05 12:00:00 +0300

    upadting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-
 import 'package:flutter/material.dart';
 
-void main (){
-  runApp(app)
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
 }
